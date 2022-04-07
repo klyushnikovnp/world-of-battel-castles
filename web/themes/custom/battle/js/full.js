877f1ed7f870b9ddfd46e326d1490041a7014e66
Nikolay <np@klyushnikov.ru>
2022-04-07 15:25:50 +0300

    Add screen 5

--- web/themes/custom/battle/js/full.js
+++ web/themes/custom/battle/js/full.js
@@ -1,21 +1,10 @@
 (function ($) {
 	
-	//@implement top-menu for bootstrap 4
-	$("ul.nav>li>a").removeClass("nav-item");
-	$("ul.nav>li>a.is-active").parent().addClass('active');
-	$("ul.nav>li>a.is-active").removeClass('is-active');
 	
-	//@dropdown menu
-	$("ul.dropdown-menu").parent("li").addClass("dropdown");
-	$("li.dropdown>a").addClass("dropdown-toggle");
-	$("li.dropdown>a").removeClass("nav-item");
-	$("li.dropdown>a").attr("href", "#");
-	$("li.dropdown>a").attr("id", "navbarDropdownMenuLink");
-	$("li.dropdown>a").attr("role", "button");
-	$("li.dropdown>a").attr("data-toggle", "dropdown");
-	$("ul.dropdown-menu>li").addClass("dropdown-item");
-	$("ul.dropdown-menu>li").removeClass("nav-item");
+	$(".carousel-item:first").addClass('active');
 	
+
+	/*fixed block*/
 	$(window).scroll(function(){
 		if ($(window).scrollTop() > 145){
 			$('.branding').addClass('position-fixed fx');
@@ -24,19 +13,6 @@
 		}
 	});	
 	
-	//@mobile menu	
-	$(".link-services").attr("href", "#");	
-	$('.link-services').click(function(){ 
-		$('.double-mobile').slideToggle(300);      
-		$('.topMenu').removeClass("d-flex");
-		$('.topMenu').css("display", "none");
-		return false;
-	}); 
-	$('.close-double-mobile').click(function(){ 
-		$('.double-mobile').slideToggle(300);      
-		$('.topMenu').slideToggle(300);      
-		return false;
-	}); 	
 
 	/*Стрелка вверх*/
 	$(window).scroll(function(){
