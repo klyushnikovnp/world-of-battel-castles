0e41149aa0f032f35eabf33010d7b6a6e71957b5
Nikolay <np@klyushnikov.ru>
2022-04-20 17:43:07 +0300

    add owl courusel

--- web/themes/custom/battle/js/full.js
+++ web/themes/custom/battle/js/full.js
@@ -2,8 +2,26 @@
 	
 	
 	$(".carousel-item:first").addClass('active');
-	
 	$(".img-circle>img").addClass('rounded-circle');
+	
+	/*install owl-carousel*/
+	$(document).ready(function(){
+		$('.owl-carousel').owlCarousel({
+			margin:10,
+			nav:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:3
+				}
+			}
+		});
+	});
 	
 
 	/*fixed block*/
